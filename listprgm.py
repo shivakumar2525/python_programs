068daebf4ba38fedd099947de59f85811e5393a2
shivakumar2525 <95363078+shivakumar2525@users.noreply.github.com>
2021-12-06 11:36:13 +0530

    Add files via upload

---
+++ listprgm.py
@@ -0,0 +1,22 @@
+
+# Creation of List
+
+# Creating a List
+List = []
+print(List)
+
+# Creating a List of numbers
+List = [10, 20, 14]
+print(List)
+
+# Creating a List of strings and accessing
+# using index
+List = ["hello", "world", "shiva"]
+print(List[0])
+print(List[2])
+
+
+
+# Creating a Multi-Dimensional List
+List = [['hello', 'shiva'] , ['world']]
+print(List)
